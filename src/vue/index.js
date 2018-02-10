bab751c76e43cd2b75e3550e46cea915e9613d15
xiaokang pu <pxk@seiue.com>
2018-02-10 19:55:06 +0800

    vue plugin

--- src/vue/index.js
+++ src/vue/index.js
@@ -1,8 +1,12 @@
+var namedAvatar = require('../index')
 var directive = require('./directive')
 
 module.exports = {
   install: function (Vue, options) {
+    namedAvatar.config(options)
+    Vue.namedAvatar = namedAvatar
 
+    Vue.directive('directive', directive)
   },
 
   directive: directive,
